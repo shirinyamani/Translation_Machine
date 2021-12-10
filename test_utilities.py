5462236ca79d9c31e0dd5df806189cfe0ae83458
Shirin Yamani <75791599+shirin1996@users.noreply.github.com>
2021-12-10 17:32:45 +0330

    Update test_utilities.py

--- test_utilities.py
+++ test_utilities.py
@@ -1,5 +1,6 @@
 import pytest 
 
+import dictionary
 def test_tokenize(sentence, dictionary, MAX_LENGTH=60):
     assert tokenize(sentence, dictionary, MAX_LENGTH=60) == [
         "how are you?: how, are, you, ?"]
